5e2e739be4ae1d42897e823a745084b072714715
WAZLL <279831132@qq.com>
2015-09-17 11:01:29 +0800

    dadad

---
+++ 99mulTable/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  99mulTable
+//
+//  Created by qianfeng on 15/1/19.
+//  Copyright (c) 2015年 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ 99mulTableTests/_9mulTableTests.m
@@ -0,0 +1,34 @@
+//
+//  _9mulTableTests.m
+//  99mulTableTests
+//
+//  Created by qianfeng on 15/1/19.
+//  Copyright (c) 2015年 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface _9mulTableTests : XCTestCase
+
+@end
+
+@implementation _9mulTableTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
