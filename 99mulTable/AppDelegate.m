6e72a63ba9a526112a99e1d51cabc195bf003ab5
WAZLL <279831132@qq.com>
2015-09-17 11:08:08 +0800

    adada

--- 99mulTable/AppDelegate.m
+++ 99mulTable/AppDelegate.m
@@ -38,7 +38,11 @@
     [self.window addSubview:view];
     [view release];
 }
-
+/**
+ *  <#Description#>
+ *
+ *  @param application <#application description#>
+ */
 - (void)applicationWillResignActive:(UIApplication *)application
 {
     // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
